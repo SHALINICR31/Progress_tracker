63e00aec08f21eb07d80171f8181fbffc344870b
CR SHALINI <mshaisha16@gmail.com>
2026-04-01 17:59:45 +0530

    Update API base URL to use environment variable

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
-const api = axios.create({ baseURL: 'http://localhost:5000/api' });
+
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api'
+});
+
 api.interceptors.request.use(cfg => {
   const t = localStorage.getItem('tf_token');
   if (t) cfg.headers.Authorization = `Bearer ${t}`;
   return cfg;
 });
+
 export default api;
