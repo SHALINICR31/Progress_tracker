5808743afdcbe17e166ee5870041e71f86ecaf45
CR SHALINI <mshaisha16@gmail.com>
2026-04-03 15:55:12 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -4,8 +4,10 @@ from flask_cors import CORS
 from routes.auth import auth_bp
 from routes.tasks import tasks_bp
 from routes.journal import journal_bp
+from storage import init_db   
 
 app = Flask(__name__)
+init_db()
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'taskflow_super_secure_key_987654')
 
 CORS(app)
